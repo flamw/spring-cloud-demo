4ad0e629d12d52701e9de1c77c94dc816b22663d
huangfeilong <huangfl@xbed.com.cn>
2019-08-06 19:31:29 +0800

    'newfile'

---
+++ spring-cloud-ribbon-openfeign/src/main/java/com/example/demo/HelloControler.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import com.example.demo.feign.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService( name );
+    }
+
+    @GetMapping(value = "/hi2")
+    public String hi2(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
